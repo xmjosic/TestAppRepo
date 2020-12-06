4d8813ed5288f730c1811f579fd3e2bedd65c66d
xmjosic <xmjosic@gmail.com>
2020-12-06 19:29:41 +0100

    Implemented 3 threads.

--- TestApp/src/hr/testapp/App.java
+++ TestApp/src/hr/testapp/App.java
@@ -1,10 +1,59 @@
 package hr.testapp;
 
+import java.util.Random;
+
 public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is a test app.");
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				int i, y = 50;
+				Random random = new Random();
+
+				i = random.nextInt();
+
+				System.out.println("Random value 1: " + i);
+
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				int i, y = 20;
+				Random random = new Random();
+
+				i = random.nextInt(y);
+
+				System.out.println("Random value 2: " + i);
+
+			}
+		});
+
+		Thread t3 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				int i, y = 10;
+				Random random = new Random();
+
+				i = random.nextInt(y);
+
+				System.out.println("Random value 3: " + i);
+
+			}
+		});
+
+		t1.start();
+		t2.start();
+		t3.start();
 
 	}
 
